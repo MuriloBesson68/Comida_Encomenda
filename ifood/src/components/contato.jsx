99120a68c411dd766f0fa53e56d01c9e0bf124a4
MuriloBesson68 <ggwp.capudo@gmail.com>
2025-04-17 08:53:37 -0300

    v4

--- ifood/src/components/contato.jsx
+++ ifood/src/components/contato.jsx
@@ -7,30 +7,34 @@ function Contato(){
    const [contato1, setContato1] = useState('')
    const [nome, setNome] = useState('')
    const [email, setEmail] = useState('')
+    const [escolaridade, setEscolaridade] = useState('')
    return(
       
          <div>
-           <h2></h2>
+           <h2>Contato</h2>
+           <br></br>
+           <br></br>
            <form>Nome:
             
             <input
+            type="text"
             value={nome}
             onChange={(e) => setNome(e.target.value)}
             placeholder= {`Digite seu ${contato1}`}
             />
             </form>
-            
-            
+            <br></br>
             <form>Email:
             <input
+            type="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             placeholder= {`Digite seu ${contato1}`}
             />
             </form>
-
-           <h3>Sexo:</h3>
-           <label>
+<br></br>
+            <form>Sexo:
+           
            <input
              type="radio"
              value="Masculino"
@@ -38,9 +42,9 @@ function Contato(){
              onChange={(e) => setContato1(e.target.value)}
            />
            Masculino
-           </label>
-
-           <label>
+           
+           
+            
            <input
              type="radio"
              value="Feminino"
@@ -48,8 +52,20 @@ function Contato(){
              onChange={(e) => setContato1(e.target.value)}
            />
          Feminino
-           </label>
+         </form>
+           <br></br>
+        <label>Escolaridade: </label>
+        <select onChange={(e) => setEscolaridade(e.target.value)} value={escolaridade}>
+          <option value="" >Selecione Uma Opção</option>
+          <option value="familia"> familia </option>
+          <option value="solo"> 1 hospedes </option>
+          <option value="casal"> 2 hospedes </option>
+        </select>
+           
+           
            </div>
+
+
    )}
 
 export default Contato
